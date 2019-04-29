6d8d57de367259561e78a355de6e46dd4b50c03e
Victor Hugo Ramos Fernandes de Santana <victor@janusproj.com>
2019-04-29 18:49:21 -0300

    criacao do pacote

--- src/Application/Service/Web3IndexerService.ts
+++ src/Application/Service/Web3IndexerService.ts
@@ -1,11 +1,40 @@
+var ipfsClient = require('ipfs-http-client');
+// import ipfsClient from 'ipfs-http-client';
+const DOMParser = require('xmldom').DOMParser;
 import IndexedHtml from '../../Domain/Entity/IndexedHtml'
 
-export default class Web3IndexerService {
 
-    IndexIpfsHostedHtml(ipfsHash: string): IndexedHtml {
-        var index = new IndexedHtml();
-        index.IpfsHash = ipfsHash;
-        index.Description = "teste";
-        return index;
+export function GetMetaTag(ipfsHtml, metaName): string {
+    let metas = ipfsHtml.getElementsByTagName('meta');
+    for (let i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('name') === metaName) {
+            return metas[i].getAttribute('content');
+        }
+    }
+    return null;
+}
+export function GetTitleValue(ipfsHtml): string {
+    let title = ipfsHtml.getElementsByTagName('title')[0];
+    return title.textContent;
+}
+
+export default class Web3IndexerService {
+    async IndexIpfsHostedHtml(ipfsHash: string) {
+        const ipfs = new ipfsClient('localhost', '5001');
+        let indexed = new IndexedHtml();
+        await ipfs.get(ipfsHash, function (err, files) {
+            files.forEach((file) => {
+                let html = file.content.toString('utf8');
+                let htmlDoc = new DOMParser().parseFromString(html, "text/xml");
+                let title = GetTitleValue(htmlDoc);
+                let tags = GetMetaTag(htmlDoc, "keywords");
+                let description = GetMetaTag(htmlDoc, "description");
+                indexed.Title = title;
+                indexed.Tags = tags;
+                indexed.Description = description;
+            });
+            return indexed;
+        });
     }
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import Web3IndexerService from './Application/Service/Web3IndexerService'
 export default class Spider {
-    // static numberRegexp = /^[0-9]+$/;
-    // public isAcceptable(s: string) {
-    //     return s.length === 5 && ZipCodeValidator.numberRegexp.test(s);
-    // }
     public IndexIpfsHostedHtml(ipfsHash: string) {
         var service = new Web3IndexerService();
         return service.IndexIpfsHostedHtml(ipfsHash);
     }
 }
+
+let spd = new Spider();
+let index = spd.IndexIpfsHostedHtml("QmTqhHXkqSAr4eEtfop37TNmanFMDdUL4W2h3muJHYMNVD");
+console.log(JSON.stringify(index));
+
+
