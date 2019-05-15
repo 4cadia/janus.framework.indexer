683c88528ff798eb06eb7774fee97e3b9e9fc29c
Victor Hugo Ramos Fernandes de Santana <victor@janusproj.com>
2019-05-15 11:44:45 -0300

    disponibilizacao spider

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
+import "reflect-metadata";
 import Bootstrapper from './Infra/IoC/Bootstrapper';
 import ISpiderService from './Application/Interface/ISpiderService';
 import IndexRequest from './Domain/Entity/IndexRequest';
 import SpiderConfig from './Domain/Entity/SpiderConfig';
-import { ContentType } from './Domain/Entity/ContentType';
 
-export default function AddContent(indexRequest: IndexRequest,
-    ownerAddress: string,
-    spiderConfig: SpiderConfig,
-    callback: any) {
-    Bootstrapper.RegisterServices(spiderConfig);
-    let spiderService = Bootstrapper.Resolve<ISpiderService>("ISpiderService");
-    spiderService.AddContent(indexRequest, ownerAddress, indexResult => {
-        callback(indexResult);
-    });
+export default class Spider {
+    _ownerAddress: string;
+    constructor(ownerAddress: string,
+        spiderConfig: SpiderConfig) {
+        this._ownerAddress = ownerAddress;
+        Bootstrapper.RegisterServices(spiderConfig);
+    }
+    AddContent(indexRequest: IndexRequest,
+        callback: any) {
+        let spiderService = Bootstrapper.Resolve<ISpiderService>("ISpiderService");
+        spiderService.AddContent(indexRequest, this._ownerAddress, indexResult => {
+            callback(indexResult);
+        });
+    }
 }
