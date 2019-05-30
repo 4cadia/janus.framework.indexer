a402e4173bf15372c5bba5e11f4ef25722f26ffe
Victor Hugo Ramos Fernandes de Santana <victor@janusproj.com>
2019-05-30 14:29:35 -0300

    comentado codigo de testes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import "reflect-metadata";
 import Bootstrapper from './Infra/IoC/Bootstrapper';
 import ISpiderService from './Application/Interface/ISpiderService';
@@ -24,30 +23,30 @@ export default class Spider {
     }
 }
 
-let connector = new MetaMaskConnector({
-    port: 3333,
-});
-let provider = connector.getProvider();
-console.log("Sign in transaction through metamask connector: http://localhost:3333");
-connector.start().then(() => {
-    let config = new SpiderConfig();
-    config.RpcHost = jsonConfig.EthereumRpcHost;
-    config.RpcPort = jsonConfig.EthereumRpcPort;
-    config.ipfsHost = jsonConfig.IpfsRpcHost;
-    config.ipfsPort = jsonConfig.IpfsRpcPort;
-    config.indexerSmAbi = jsonConfig.indexerSmAbi;
-    config.indexerSmAddress = jsonConfig.indexerSmAddress;
-    config.Web3Provider = provider;
+// let connector = new MetaMaskConnector({
+//     port: 3333,
+// });
+// let provider = connector.getProvider();
+// console.log("Sign in transaction through metamask connector: http://localhost:3333");
+// connector.start().then(() => {
+//     let config = new SpiderConfig();
+//     config.RpcHost = jsonConfig.EthereumRpcHost;
+//     config.RpcPort = jsonConfig.EthereumRpcPort;
+//     config.ipfsHost = jsonConfig.IpfsRpcHost;
+//     config.ipfsPort = jsonConfig.IpfsRpcPort;
+//     config.indexerSmAbi = jsonConfig.indexerSmAbi;
+//     config.indexerSmAddress = jsonConfig.indexerSmAddress;
+//     config.Web3Provider = provider;
 
-    let indexRequest = new IndexRequest();
-    indexRequest.Content = "C:\\Users\\Victor Hugo Ramos\\Downloads\\TesteVictor\\Teste123.zip";
-    indexRequest.ContentType = ContentType.Zip;
-    Bootstrapper.RegisterServices(config);
-    let spiderService = Bootstrapper.Resolve<ISpiderService>("ISpiderService");
-    spiderService.AddContent(indexRequest, "0xB8C0DF194E38EeF45F36Bd8fBbe41893ccc16D20", indexResult => {
-        console.log(indexResult);
-    });
-});
+//     let indexRequest = new IndexRequest();
+//     indexRequest.Content = "C:\\Users\\Victor Hugo Ramos\\Downloads\\TesteVictor\\Teste123.zip";
+//     indexRequest.ContentType = ContentType.Zip;
+//     Bootstrapper.RegisterServices(config);
+//     let spiderService = Bootstrapper.Resolve<ISpiderService>("ISpiderService");
+//     spiderService.AddContent(indexRequest, "0xB8C0DF194E38EeF45F36Bd8fBbe41893ccc16D20", indexResult => {
+//         console.log(indexResult);
+//     });
+// });
 
 
 
